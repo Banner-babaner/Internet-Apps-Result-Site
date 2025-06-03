d54bc66dba690053e0bf3e6ac10f497dad652d8d
ShadowFrog <vitalyarychagov@yandex.ru>
2025-06-04 00:07:57 +0400

    themeSwitcher

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ document.documentElement.classList.toggle(
   'dark',
   localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
 )
+document.getElementById("changer").onclick = changeTheme;
+document.getElementById("mini-changer").onclick = changeTheme;
 
+function changeTheme(){
 
+  localStorage.setItem("theme", document.documentElement.classList.toggle('dark')?"dark":"light");
+}
