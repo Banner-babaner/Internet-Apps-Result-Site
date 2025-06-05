a92e3da2a72df61937c1d579185e71761200369d
ShadowFrog <vitalyarychagov@yandex.ru>
2025-06-05 16:08:30 +0400

    test

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ const userData = document.querySelector("#userData")
 const userName = document.querySelector("#userName");
 const userScore = document.querySelector("#userScore");
 
+let date = new Date();
+document.querySelector("#days").innerText = date.getDay();
+document.querySelector("#hours").innerText = date.getHours();
+document.querySelector("#minutes").innerText = date.getMinutes();
+document.querySelector("#seconds").innerText = date.getSeconds();
+
+
+setInterval(
+  ()=>{
+    let date = new Date();
+    document.querySelector("#days").innerText = date.getDay();
+    document.querySelector("#hours").innerText = date.getHours();
+    document.querySelector("#minutes").innerText = date.getMinutes();
+    document.querySelector("#seconds").innerText = date.getSeconds();
+  },
+  1000
+)
+
 if(user){
   userEnter.classList.remove("animate-pulse");
   userEnter.innerText = user;
@@ -33,15 +51,10 @@ document.querySelector("#commitName").onclick = ()=>{
   userName.innerText = user;
   if(localStorage.getItem(user)) userScore.innerText = localStorage.getItem(user);
   userEnter.onclick = showUserData;
+  clearTest();
   autorization.close();
 }
 document.querySelector("#cancelName").onclick = ()=>{
-  localStorage.removeItem("user");
-  userEnter.classList.add("animate-pulse");
-  userEnter.innerText = "Зайти в кабинет";
-  userEnter.onclick = ()=>{
-    autorization.showModal();
-  }
   autorization.close();
 }
 
@@ -53,6 +66,7 @@ document.querySelector("#exit").onclick = ()=>{
     autorization.showModal();
   }
   userData.close();
+  clearTest();
 }
 document.querySelector("#burgerButton").onclick = ()=>{
     document.querySelector("#burger").classList.toggle("hidden");
@@ -73,6 +87,7 @@ document.querySelector("#closeData").onclick = ()=>{
   userData.close();
 }
 
+document.querySelector("#checkTest").onclick=checkTest;
 
 
 
@@ -96,4 +111,44 @@ function changeTheme(){
 
 function showUserData(){
   userData.showModal();
+}
+
+
+function clearTest(){
+  for(let i=0; i<10; i++){
+    let select = document.querySelector(`#question${i+1}`);
+    document.querySelector(`#question${i+1}`).disabled=false;
+    document.querySelector(`#question${i+1}`).value = 0;
+    select.classList.remove("bg-red-400");
+    select.classList.remove("bg-green-400");
+    select.title = "";
+  }
+  document.querySelector("#checkTest").innerText="Проверить";
+  document.querySelector("#checkTest").onclick = checkTest;
+}
+
+function checkTest(){
+  let rights = [1, 3, 4, 2, 3, 1, 1, 2, 4, 1];
+  let res=0;
+  for(let i=0; i<10; i++){
+    let select = document.querySelector(`#question${i+1}`);
+    if(select.value==rights[i]){
+      res++;
+      select.classList.add("bg-green-400");
+      select.title = "Верно";
+    }
+    else{
+      select.classList.add("bg-red-400");
+      select.title = "Неверно";
+    }
+    select.disabled = true;
+  }
+  if(user){
+    if(res>localStorage.getItem(user)){
+      localStorage.setItem(user, res);
+    }
+  }
+  document.querySelector("#checkTest").innerText="Попробовать снова";
+  document.querySelector("#checkTest").onclick = clearTest;
+  alert(`Ваш результат: ${res}${user?"":"\nАвторизуйтесь, чтобы сохранить его!"}`);
 }
